bca84bf3ee2f5b781487ba88efa4823cf025a758
Adityapk2002 <adityakhandade1111@gmail.com>
2026-02-17 22:12:26 +0530

    learned layouts

---
+++ ai-builder/src/App.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+import MainLayout from "./app/layout/MainLayout";
+
+function App() {
+  return (
+    <>
+      <div>
+        <MainLayout />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ ai-builder/src/app/layout/MainLayout.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const MainLayout = () => {
+  const [activeTab, setActiveTab] = useState<"code" | "preview">("preview");
+
+  return (
+    <div className="h-screen w-full bg-[#111111] text-white flex overflow-hidden">
+      <div className="flex flex-col w-[40%] border-r border-white bg-[#0d0d0d]">
+        <div className="flex items-center ont-medium pl-12 border-b py-2 gap-2 font-cinzel text-lg">
+          <span className="bg-yellow-300 h-4 w-4 rounded-full" />
+          Lovable
+        </div>
+        <div className="flex-1 p-4 overflow-auto text-white">
+          Chat messages will come here
+        </div>
+
+        <div className="p-3 border-t border-white">
+          <input
+            type="text"
+            placeholder="Ask Anything to AI"
+            className="w-full rounded-md outline-none px-4 py-2"
+          />
+        </div>
+      </div>
+
+      <div className="w-[60%] flex flex-col">
+        <div className="h-14 border-b border-white flex items-center justify-between px-4 bg-[#0f0f0f]">
+          <div className="flex gap-2">
+            <button
+              onClick={() => setActiveTab("code")}
+              className={`px-4 py-1 rounded-md text-sm ${activeTab == "code" ? "bg-white text-black " : "text-white/60"}`}
+            >
+              Code
+            </button>
+            <button
+              onClick={() => setActiveTab("preview")}
+              className={`px-4 py-1 rounded-md text-sm ${activeTab == "preview" ? "bg-white text-black " : "text-white/60"}`}
+            >
+              Preview
+            </button>
+          </div>
+
+          <div className="text-sm text-white">Aditya</div>
+        </div>
+
+        <div className="flex-1 bg-black flex items-center justify-center">
+          {activeTab == "code"
+            ? "Code editor coming..."
+            : "Live preview coming..."}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
